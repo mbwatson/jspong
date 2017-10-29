c900eaab08609a95ba1d72ebd4f964b66d3e336b
mwatson <mattwat@gmail.com>
2017-10-29 13:45:29 -0400

    comments

--- js/game.js
+++ js/game.js
@@ -27,17 +27,19 @@ function draw() {
 }
 
 function keyPressed() {
+	// players' controls
   if (keyCode === UP_ARROW) { game.p1.paddle.moveUp(); }
   if (keyCode === DOWN_ARROW) { game.p1.paddle.moveDown(); }
   if (key == 'A') { game.p2.paddle.moveUp(); }
   if (key == 'Z') { game.p2.paddle.moveDown(); }
+  // game control
   if (keyCode === ENTER) {
   	game.ball.kill();
   	game.ball = new Ball(width/2, height/2);
   	game.hits = 0;
   }
-  if (key == 'D') { game.debug = !game.debug; }
 	if (key === ' ') { game.paused = !game.paused; }
+  if (key == 'D') { game.debug = !game.debug; }
 }
 
 function approx(val, n) {
